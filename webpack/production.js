cf90377429bc652f2bf5cfe00c63370f28397839
Kevin Velasco <me@kvnvelasco.com>
2018-02-08 23:11:29 +0800

    add missing plugins to production configuration

--- webpack/production.js
+++ webpack/production.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack')
 module.exports = {
   entry: {
-    application: ['babel-polyfill', 'react-hot-loader/patch', path.resolve(process.cwd(), './index.tsx')],
+    application: ['babel-polyfill', path.resolve(process.cwd(), './index.tsx')],
     vendor: common.entryPoint.vendor
   },
   output: common.output,
@@ -12,6 +12,13 @@ module.exports = {
     rules: common.rules,
   },
   plugins: [
+    new HTMLPlugin({
+      template: path.resolve(process.cwd(), './public/index.html')
+    }),
+    new TsConfigPathsPlugin({
+      tsconfig: path.resolve(process.cwd(), "tsconfig.json"),
+      compiler: "typescript"
+    }),
     new webpack.DefinePlugin({ // <-- key to reducing React's size
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
